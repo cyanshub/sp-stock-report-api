aa7abdf6e8da33f79d83f8b4a4cb7372b0312dbf
travel15740 <travel15740@gmail.com>
2025-01-10 23:48:34 +0800

    feat: 構建專案的路由架構與報錯系統

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// 載入 controller
+const stockController = require('../controllers/stock-controller')
+
+// 載入 middleware
+const { apiErrorHandler } = require('../middleware/error-handler')
+
+// 設計路由: 主要功能
+router.get('/', (req, res, next) => res.send('Stock report app is running.'))
+
+// 設計路由: 錯誤處理
+router.use('/', apiErrorHandler)
+
+module.exports = router
